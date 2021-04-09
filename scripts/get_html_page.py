ebdce7182651f3c118a4c76d2a557fddf1dfa949
KatalinNagy <katalin.nagy.nk@gmail.com>
2021-04-09 12:39:32 +0200

    Adding script to download html pages for local testing

--- scripts/get_html_page.py
+++ scripts/get_html_page.py
@@ -1,5 +1,21 @@
 import sys
+import scrapy
 from scrapy.crawler import CrawlerProcess
 
-print(sys.argv[0])
 
+class HtmlDownloadSpider(scrapy.Spider):
+    name = "html_download_spider"
+    start_urls = [sys.argv[1]]
+
+    def parse(self, response):
+        path = 'html_pages/'
+        filename = path + sys.argv[1].split('/')[-2] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
+
+c = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/5.0',
+})
+c.crawl(HtmlDownloadSpider)
+c.start()
